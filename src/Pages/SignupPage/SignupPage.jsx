7633b06612fbf5a45d0293cecdc759c628abf9bb
TheGoldDemon <the6gold9demon@gmail.com>
2025-11-26 05:31:41 +0300

    Update API URL and clean up SignupPage component

--- src/Pages/SignupPage/SignupPage.jsx
+++ src/Pages/SignupPage/SignupPage.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 import styles from "./SignupPage.module.css";
 
-const API_URL = "https://balsam-beta-backend-production-aa55.up.railway.app";
+const API_URL = "https://balsam-beta-backend.onrender.com";
 //===================================================================================//
 // API Function
 async function CreateAccount(data) {
@@ -156,3 +156,4 @@ function SignupPage() {
 // Export
 export default SignupPage;
 //===================================================================================//
+
